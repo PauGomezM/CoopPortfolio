58c5a1f414e298d47932a0fd43e6da446e1809f4
Aleix-PC <aleix.fort45@gmail.com>
2023-03-18 23:25:29 +0100

    Added features
    Reformatted dialogue boxes, added hovering functionality, and reconsidered hover actions

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 for (i=0; i<2; i++){
 const pic = document.querySelectorAll('.profile_pic')[i];
+const dialogue_box = document.querySelectorAll('.dialogue_box')[i]
+
+    dialogue_box.style.visibility = 'hidden';
 
     pic.addEventListener('mouseover', () => {
         pic.classList.add('stay_up');
+        dialogue_box.style.visibility = 'visible';
     })
 
     pic.addEventListener('mouseout', () => {
-            pic.classList.remove('stay_up');
+        pic.classList.remove('stay_up');
+        dialogue_box.style.visibility = 'hidden';
     })
-
 }
+
+
+
+
