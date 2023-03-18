0d81e7631492aa4f3f449a2e33a17bfc7382f78f
Aleix-PC <aleix.fort45@gmail.com>
2023-03-18 22:42:24 +0100

    Added dialogue boxes

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
-const pic = document.querySelector('.profile_pic');
+for (i=0; i<2; i++){
+const pic = document.querySelectorAll('.profile_pic')[i];
 
+    pic.addEventListener('mouseover', () => {
+        pic.classList.add('stay_up');
+    })
 
-pic.addEventListener('mouseover', () => {
-    pic.classList.add('stay_up');
-})
+    pic.addEventListener('mouseout', () => {
+            pic.classList.remove('stay_up');
+    })
 
-pic.addEventListener('mouseout', () => {
-        pic.classList.remove('stay_up');
-})
-
-const pic2 = document.querySelectorAll('.profile_pic')[1];
-
-pic2.addEventListener('mouseover', () => {
-    pic2.classList.add('stay_up');
-})
-
-pic2.addEventListener('mouseout', () => {
-        pic2.classList.remove('stay_up');
-})
+}
