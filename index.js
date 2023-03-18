c063781746b316086ac13531595ca5713c55ac0c
Aleix-PC <aleix.fort45@gmail.com>
2023-03-18 19:34:25 +0100

    Fixes
    fixed padding width and changed names to capital letters

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-/*
-for (var i = 0; i<2; i++){
-    document.querySelectorAll('.profile_pic')[i].addEventListener('mouseover', picHover)
-}
-
-
-function picHover() {
-    
-}
-
-*/
-
 const pic = document.querySelector('.profile_pic');
 
 
@@ -30,28 +18,3 @@ pic2.addEventListener('mouseover', () => {
 pic2.addEventListener('mouseout', () => {
         pic2.classList.remove('stay_up');
 })
-/*
-pic.addEventListener('animationend', () => {
-    if (state == 0){
-        pic.classList.add('stay_up');
-        pic.classList.remove('play_up');
-        state = 1
-    }
-
-    if (state == 1){
-        pic.classList.remove('play_down')
-        pic.classList.add('stay_down')
-        state = 0
-    }    
-})
-
-/*
-pic.addEventListener('animationend', () => {
-    if (state == 1){
-        pic.classList.remove('play_down')
-        pic.classList.add('stay_down')
-        state = 0
-    }
-})
-*/
-/******************When click Aleix*********************************/
