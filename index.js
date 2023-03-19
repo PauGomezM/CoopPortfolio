8e042bf27180f79adf5dc423bd71bffdfc71d34e
Aleix-PC <aleix.fort45@gmail.com>
2023-03-19 04:37:18 +0100

    Fixed bugs, added decorations

--- index.js
+++ index.js
@@ -1,44 +1,31 @@
+/* ---- hover pictures ----  By changing opacity to be 0 by default, and adding class show
+instead of hide, we can avoid the dialogue boxes appearing every time the page reloads*/ 
+for (i=0; i<2; i++){
+    const pic = document.querySelectorAll('.profile_pic')[i];
+    const dialogue_box = document.querySelectorAll('.dialogue_box')[i]
+
+
+    pic.addEventListener('mouseover', () => {
+        pic.classList.add('stay_up');
+        dialogue_box.classList.toggle('shown');
+    })
+
+    pic.addEventListener('mouseout', () => {
+        pic.classList.remove('stay_up');
+        dialogue_box.classList.toggle('shown');
+    })
+
+}
+
+/* ---- Scroll window smoothly ---- */
+const home_button = document.querySelector('.navbar-links');
+home_button.addEventListener('mouseover', scrollFunc)
+
+function scrollFunc(){
+    window.scrollTo(0, 1200);
+}
 
 /*
-for (i=0; i<2; i++){
-    const pic = document.querySelectorAll('.profile_pic')[i];
-    const dialogue_box = document.querySelectorAll('.dialogue_box')[i]
-    
-        dialogue_box.style.visibility = 'hidden';
-    
-        pic.addEventListener('mouseover', () => {
-            pic.classList.add('stay_up');
-            dialogue_box.style.visibility = 'visible';
-            dialogue_box.classList.toggle('hidden');
-        })
-
-        pic.addEventListener('mouseout', () => {
-            pic.classList.remove('stay_up');
-            dialogue_box.style.visibility = 'hidden';
-        })
-    }
+var el = document.querySelector('.dialogue_box');
+el.style.display = "none";
 */
-
-for (i=0; i<2; i++){
-    const pic = document.querySelectorAll('.profile_pic')[i];
-    const dialogue_box = document.querySelectorAll('.dialogue_box')[i]
-    
-        dialogue_box.classList.toggle('hidden');
-
-        pic.addEventListener('mouseover', () => {
-            pic.classList.add('stay_up');
-            dialogue_box.classList.toggle('hidden');
-        })
-
-        pic.addEventListener('mouseout', () => {
-            pic.classList.remove('stay_up');
-            dialogue_box.classList.toggle('hidden');
-        })
-    }
-
-    const home_button = document.querySelector('.navbar-links');
-    home_button.addEventListener('mouseover', scrollFunc)
-
-    function scrollFunc(){
-        window.scrollTo(0, 1200);
-    }
